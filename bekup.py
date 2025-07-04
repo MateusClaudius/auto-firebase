4b2c9e6735e8feaac5d88392cf34adeeca4b0d43
MateusClaudius <gmateusclaudius@gmail.com>
2025-07-04 12:08:23 -0300

    excluindo

--- bekup.py
+++
@@ -1,6 +0,0 @@
-import os
-for arquivo in os.listdir(f'c:\\Users\\mateus.santos_acesso\\Downloads'):
-    if 'json' in arquivo:
-        print(arquivo)
-        os.rename(f'c:\\Users\\mateus.santos_acesso\\Downloads\\{arquivo}', f'C:\\Users\\mateus.santos_acesso\\Documents\\repositorios\\auto-firebase\\firebase\\{arquivo[:13].title() + '.json'}')
-
--- teste.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('path.csv', 'r', newline='', encoding='utf-8') as arquivo:
-
-    leitor = csv.reader(arquivo, delimiter=';')
-
-    for indice, linha in enumerate(leitor):
-        print(indice + 1, linha)
